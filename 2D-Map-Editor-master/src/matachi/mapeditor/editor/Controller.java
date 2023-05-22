a3260f1e2d2e439cf2e77099be238c836357a83e
cxlanyagege <cxlanyagege@hotmail.com>
2023-05-22 12:25:46 +1000

    add game start button from lms

--- 2D-Map-Editor-master/src/matachi/mapeditor/editor/Controller.java
+++ 2D-Map-Editor-master/src/matachi/mapeditor/editor/Controller.java
@@ -96,6 +96,8 @@ public class Controller implements ActionListener, GUIInformation {
 			loadFile();
 		} else if (e.getActionCommand().equals("update")) {
 			updateGrid(gridWith, gridHeight);
+		} else if (e.getActionCommand().equals("start_game")) {
+			// TODO: Code to switch to pacman game
 		}
 	}
 
