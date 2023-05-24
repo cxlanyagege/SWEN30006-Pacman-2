8e507c7f411bdb89631ebda852d9a2ea76b273fc
Kerr <kerrrrryuan@gmail.com>
2023-05-25 00:41:17 +1000

    add gameCheck

--- pacman/src/matachi/mapeditor/editor/Controller.java
+++ pacman/src/matachi/mapeditor/editor/Controller.java
@@ -233,7 +233,7 @@ public class Controller implements ActionListener, GUIInformation {
                     this.loadContext.setStrategy(folderLoadStrategy);
 
                     loadContext.load(selectedFile);
-                    //TODO: Game Check
+                    gameCheck(selectedFile);
 
                 } else if (selectedFile.canRead() && selectedFile.exists()) {
                     currentFileName = selectedFile.getName();
@@ -259,6 +259,65 @@ public class Controller implements ActionListener, GUIInformation {
     public Tile getSelectedTile() {
         return selectedTile;
     }
+
+    // Game Checking
+    private void gameCheck(File gameFolder) {
+        File[] mapFiles = gameFolder.listFiles((dir, name) -> name.endsWith(".xml"));
+
+        // 检查是否至少有一个正确命名的地图文件
+        if (mapFiles == null || mapFiles.length == 0) {
+            String message = String.format("[Game %s – no maps found]", gameFolder.getName());
+            System.out.println(message);
+            writeToLogFile(message);
+            return;
+        }
+
+        // 检查地图文件序列是否良好定义
+        checkMapFileSequence(mapFiles);
+    }
+
+    private void checkMapFileSequence(File[] mapFiles) {
+        Map<String, List<String>> mapLevelFiles = new HashMap<>();
+
+        // 将地图文件按等级分组
+        for (File file : mapFiles) {
+            String fileName = file.getName();
+            String level = getLevelFromFileName(fileName);
+
+            List<String> levelFiles = mapLevelFiles.getOrDefault(level, new ArrayList<>());
+            levelFiles.add(fileName);
+            mapLevelFiles.put(level, levelFiles);
+        }
+
+        // 检查每个等级的地图文件数量
+        for (Map.Entry<String, List<String>> entry : mapLevelFiles.entrySet()) {
+            String level = entry.getKey();
+            List<String> levelFiles = entry.getValue();
+
+            if (levelFiles.size() > 1) {
+                String message = String.format("[Game %s – multiple maps at same level: %s]",
+                        currentFileName, String.join("; ", levelFiles));
+                System.out.println(message);
+                writeToLogFile(message);
+            }
+        }
+    }
+
+    private String getLevelFromFileName(String fileName) {
+        StringBuilder levelBuilder = new StringBuilder();
+
+        for (int i = 0; i < fileName.length(); i++) {
+            char ch = fileName.charAt(i);
+            if (Character.isDigit(ch)) {
+                levelBuilder.append(ch);
+            } else {
+                break;
+            }
+        }
+
+        return levelBuilder.toString();
+    }
+
 
     // Level Checking
 
@@ -481,13 +540,22 @@ public class Controller implements ActionListener, GUIInformation {
 
     private void writeToLogFile(String message) {
         try {
-            String logFileName = currentFileName + "_log.txt"; // log文件名为当前文件名加上后缀.log
-            FileWriter writer = new FileWriter(new File(fileDirectory, logFileName), true);
+            String logDirectoryName = "logDocument"; // 日志文件夹名称
+            File logDirectory = new File(System.getProperty("user.dir"), logDirectoryName);
+            if (!logDirectory.exists()) {
+                logDirectory.mkdir(); // 创建日志文件夹
+            }
+
+            String logFileName = currentFileName + "_log.txt"; // 日志文件名为当前文件名加上后缀.log
+            File logFile = new File(logDirectory, logFileName);
+
+            FileWriter writer = new FileWriter(logFile, true);
             writer.write(message + "\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
 }
